de350475332efd49f0254bc2936d49161d5d4538
Aako <126260482+Shikiaa@users.noreply.github.com>
2024-09-21 11:14:27 +0800

    添加了注册功能

---
+++ src/main/java/dao/SystemAdminDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import entity.SystemAdmin;
+
+import java.sql.SQLException;
+
+public interface SystemAdminDao {
+
+    public SystemAdmin findByUsername(String username) throws SQLException;
+
+    public SystemAdmin insertNewUser(String username,String password) throws SQLException;
+}
